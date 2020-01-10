291fd5a6fd08c5531e91cb2689a99791d6ca9c98
Igor Borges <igor@borges.dev>
2020-01-10 00:35:24 -0300

    Old updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,24 +5,18 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function customDoIt() {
-	git config --global user.email "igor@borges.me"
+	git config --global user.email "igor@borges.dev"
 	git config --global user.name "Igor Borges"
 	git config --global user.signingkey "7AD24624"
 	git config --global core.excludesfile "~/.gitignore"
 
 	mkdir -p "$HOME/.oh-my-zsh/themes"
-  cp "igor.zsh-theme" "$HOME/.oh-my-zsh/themes/"
+  	cp "igor.zsh-theme" "$HOME/.oh-my-zsh/themes/"
 
-  mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-	cp -R "sublime/" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
-
-	mkdir -p "$HOME/.config/kitty"
-	cp "kitty.conf" "$HOME/.config/kitty/"
 
 	mkdir -p "$HOME/Library/Application Support/Code/User"
 	cp -R "vscode/" "$HOME/Library/Application Support/Code/User/"
-
-	curl 'https://github.com/robbyrussell/oh-my-zsh/compare/master...Igor1201:magic-patch.patch' > /tmp/magic.patch
+	curl 'https://github.com/robbyrussell/oh-my-zsh/compare/master...comigor:magic-patch.patch' > /tmp/magic.patch
 	( cd "$HOME/.oh-my-zsh"; git apply /tmp/magic.patch )
 }
 
