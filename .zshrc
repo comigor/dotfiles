bf581cf4d43f7cbe8749b99daed1305c50139e95
Igor Borges <igor@borges.dev>
2020-01-26 15:53:04 -0300

    Update some stuff

--- .zshrc
+++ .zshrc
@@ -1,15 +1,41 @@
 export ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="spaceship"
-SPACESHIP_KUBECONTEXT_SHOW=false
+SPACESHIP_TIME_SHOW=true
+
 SPACESHIP_PACKAGE_SHOW=false
 SPACESHIP_NODE_SHOW=false
+SPACESHIP_RUBY_SHOW=false
+SPACESHIP_ELM_SHOW=false
+SPACESHIP_ELIXIR_SHOW=false
+SPACESHIP_XCOODE_SHOW=false
+SPACESHIP_GOLANG_SHOW=false
+SPACESHIP_PHP_SHOW=false
+SPACESHIP_RUST_SHOW=false
+SPACESHIP_HASKELL_SHOW=false
+SPACESHIP_JULIA_SHOW=false
+SPACESHIP_DOCKER_SHOW=false
+SPACESHIP_DOCKER_CONTEXT_SHOW=false
+SPACESHIP_AWS_SHOW=false
+SPACESHIP_VENV_SHOW=false
+SPACESHIP_CONDA_SHOW=false
+SPACESHIP_PYENV_SHOW=false
+SPACESHIP_DOTNET_SHOW=false
+SPACESHIP_EMBER_SHOW=false
+SPACESHIP_KUBECTL_SHOW=false
+SPACESHIP_KUBECTL_VERSION_SHOW=false
+SPACESHIP_KUBECONTEXT_SHOW=false
+SPACESHIP_TERRAFORM_SHOW=false
 
 plugins=(gitfast osx)
 
 source $ZSH/oh-my-zsh.sh
 
 autoload -U add-zsh-hook
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+precmd() {
+    vcs_info
+}
 
-# for some reason this doesn't work on .zprofile
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,45 +1,43 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-cd "$(dirname "${BASH_SOURCE}")";
+rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".macos" \
+    --exclude ".ubuntu" \
+    --exclude "bootstrap.sh" \
+    --exclude "README.md" \
+    --exclude "LICENSE-MIT.txt" \
+    --exclude "igor.zsh-theme" \
+    --exclude "brew.sh" \
+    --exclude "sublime" \
+    --exclude "vscode" \
+    --exclude "kitty.conf" \
+    -avh --no-perms . ~;
 
-git pull origin master;
+git config --global user.email "igor@borges.dev"
+git config --global user.name "Igor Borges"
+git config --global user.signingkey "7AD24624"
+git config --global core.excludesfile "~/.gitignore"
 
-function customDoIt() {
-	git config --global user.email "igor@borges.dev"
-	git config --global user.name "Igor Borges"
-	git config --global user.signingkey "7AD24624"
-	git config --global core.excludesfile "~/.gitignore"
+mkdir -p "$HOME/.oh-my-zsh/themes"
+cp "igor.zsh-theme" "$HOME/.oh-my-zsh/themes/"
 
-	mkdir -p "$HOME/.oh-my-zsh/themes"
-  	cp "igor.zsh-theme" "$HOME/.oh-my-zsh/themes/"
+mkdir -p "$HOME/Library/Application Support/Code - Insiders/User"
+rm -f $HOME/Library/Application\ Support/Code\ -\ Insiders/User/*.json
+ln -s $PWD/vscode/* "$HOME/Library/Application Support/Code - Insiders/User"
 
-	mkdir -p "$HOME/Library/Application Support/Code - Insiders/User"
-	cp -R "vscode/" "$HOME/Library/Application Support/Code - Insiders/User/"
+curl -L 'https://github.com/robbyrussell/oh-my-zsh/compare/master...comigor:magic-patch.patch' > /tmp/magic.patch
+( cd "$HOME/.oh-my-zsh"; git apply /tmp/magic.patch || true )
 
-	curl 'https://github.com/robbyrussell/oh-my-zsh/compare/master...comigor:magic-patch.patch' > /tmp/magic.patch
-	( cd "$HOME/.oh-my-zsh"; git apply /tmp/magic.patch )
-}
+# Fonts
+FIRACODE=$(mktemp -d)
+git clone git@github.com:powerline/fonts.git $FIRACODE
+( cd "$FIRACODE/FiraMono"; cp *.otf $HOME/Library/Fonts/ )
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".macos" \
-		--exclude ".ubuntu" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		--exclude "igor.zsh-theme" \
-		--exclude "brew.sh" \
-		--exclude "sublime" \
-		--exclude "vscode" \
-		--exclude "kitty.conf" \
-		-avh --no-perms . ~;
+JETBRAINSMONO=$(mktemp -d)
+git clone git@github.com:JetBrains/JetBrainsMono.git $JETBRAINSMONO
+( cd "$JETBRAINSMONO/ttf"; cp *.ttf $HOME/Library/Fonts/ )
 
-	customDoIt;
-
-	source ~/.zprofile;
-}
-
-doIt;
-unset doIt;
-unset customDoIt;
+clear
+zsh
