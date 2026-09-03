b21471f3313b7092d19e6c695654502473e0bb32
Igor Borges <igor@borges.dev>
2026-09-03 12:35:37 -0300

    Cut staging from prod tiers; default prod tier to decade-prod

--- configs/envbox.d/prod.zsh
+++ configs/envbox.d/prod.zsh
@@ -1,6 +1,7 @@
 ENVBOX_LOCK_RBW=1
 
 export ENVIRONMENT=prod
+export AWS_PROFILE=decade-prod
 
 cfgfile ~/.aws/config ~/.config/envbox.d/aws-config.prod
 cfgfile ~/.kube/config ~/.config/envbox.d/kubeconfig.prod
