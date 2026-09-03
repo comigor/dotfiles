77856563fe16a1ed39c0d70b36c90fd37147d5a6
Igor Borges <igor@borges.dev>
2026-09-03 12:22:25 -0300

    Wire per-tier kubeconfigs into envbox (staging-only base, prod inside prod-admin)

--- configs/envbox.d/prod-admin.zsh
+++ configs/envbox.d/prod-admin.zsh
@@ -5,3 +5,4 @@ export ENVIRONMENT=prod-admin
 export AWS_PROFILE=decade-prod
 
 cfgfile ~/.aws/config ~/.config/envbox.d/aws-config.prod-admin
+cfgfile ~/.kube/config ~/.config/envbox.d/kubeconfig.prod-admin
--- configs/envbox.d/staging.zsh
+++ configs/envbox.d/staging.zsh
@@ -1,3 +1,4 @@
 export ENVIRONMENT=staging
 
 cfgfile ~/.aws/config ~/.config/envbox.d/aws-config.staging
+cfgfile ~/.kube/config ~/.config/envbox.d/kubeconfig.staging
